6f3cf0f6ddd1adb981c12b8bde2aff129c7dcf11
Zadok Armah <zadok@Zadoks-MacBook-Pro.local>
2018-11-27 11:41:02 +0000

    Second Commit

--- controllers/default.py
+++ controllers/default.py
@@ -1,3 +1,27 @@
 
 def addAccount():
     return dict()
+
+
+def save():
+    submitted_account_name = request.vars.account_name
+    submitted_account_number = request.vars.account_number
+    submitted_bank = request.vars.bank
+    submitted_branch = request.vars.branch
+
+    results = db.users.insert(
+            db_account_name = submitted_account_name,
+            db_account_number = submitted_account_number,
+            db_bank = submitted_bank,
+            db_branch = submitted_branch
+    )
+
+    if results:
+        return "User saved successfully"
+    else:
+        return "An Error Occured. Please check and try again."
+
+
+def viewAccounts():
+    accounts = db().select(db.users.ALL)
+    return dict(users=accounts)        
